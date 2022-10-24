71bf678c6070248f050be1160a44a2d2d4290ffa
SardorDev12 <wiut00012453@gmail.com>
2022-10-24 20:38:12 +0500

    feat: add preloader

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const video = document.querySelector(".video-container");
 const controlBtn = document.querySelector(".switch-btn");
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", () => {
+  preloader.classList.add("hide-preloader");
+});
 
 controlBtn.addEventListener("click", () => {
   if (!controlBtn.classList.contains("play")) {
